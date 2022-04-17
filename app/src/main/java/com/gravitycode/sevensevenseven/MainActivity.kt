464b53cfc9d31d42286676fbff6497f75586dc48
Adam Buicke <buickea@gmail.com>
2022-04-17 20:14:20 +0100

    Working raw json parsing

--- app/src/main/java/com/gravitycode/sevensevenseven/MainActivity.kt
+++ app/src/main/java/com/gravitycode/sevensevenseven/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.gravitycode.sevensevenseven
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val jsonInputStream: InputStream = resources.openRawResource(R.raw.sevensevenseven)
+
+        val bytes = ByteArray(jsonInputStream.available())
+        jsonInputStream.read(bytes, 0, bytes.size)
+
+        val jsonString = String(bytes)
+        val jsonArray = JSONArray(jsonString)
+
+        val row1 = jsonArray[1]
+        Log.i("mojo", row1.toString())
     }
 }
