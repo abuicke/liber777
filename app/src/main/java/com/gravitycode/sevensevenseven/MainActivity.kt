06b74be5a46a727dc8261b650eba758af9737004
Adam Buicke <buickea@gmail.com>
2022-04-21 16:34:07 +0100

    cleaned up alertdialog logic

--- app/src/main/java/com/gravitycode/sevensevenseven/MainActivity.kt
+++ app/src/main/java/com/gravitycode/sevensevenseven/MainActivity.kt
@@ -5,42 +5,42 @@ import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.gravitycode.sevensevenseven.databinding.MainActivityBinding
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
-    private lateinit var lookupView: LookupView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = MainActivityBinding.inflate(layoutInflater)
-        lookupView = LookupView(this)
-
         setContentView(binding.root)
 
         val liber777 = Liber777(this)
 
         binding.dropdownRows.onItemSelectedListener = OnItemSelectedListener { text, position ->
             if (position > 0) {
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle(text)
-                builder.setItems(liber777.getRow(position - 1).map()) { dialog, which ->
-                    // TODO: Open LookupView here
+                show(text, liber777.getRow(position - 1)) {
+                    // show()
                 }
-                builder.show()
             }
         }
 
         binding.dropdownColumns.onItemSelectedListener = OnItemSelectedListener { text, position ->
             if (position > 0) {
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle(text)
-                builder.setItems(liber777.getColumn(position - 1).map()) { dialog, which ->
-                    // TODO: Open LookupView here
+                show(text, liber777.getColumn(position - 1)) {
+                    // show()
                 }
-                builder.show()
             }
         }
+    }
+
+    private fun show(title: String, jsonArray: JSONArray, onClick: (Int) -> Unit) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setItems(jsonArray.map()) { _, which ->
+            onClick(which)
+        }
+        builder.show()
     }
 }
